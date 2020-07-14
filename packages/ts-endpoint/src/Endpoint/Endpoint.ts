aa988bf072d73529c0e86dbac58e504bd63935a3
fes300 <federicosordillo@gmail.com>
2020-07-14 11:38:27 +0200

    feat: add get static path util

--- packages/ts-endpoint/src/Endpoint/Endpoint.ts
+++ packages/ts-endpoint/src/Endpoint/Endpoint.ts
@@ -1,4 +1,5 @@
 import * as t from 'io-ts';
+import { identity } from 'fp-ts/lib/function';
 
 export const HTTPMethod = t.keyof(
   {
@@ -51,8 +52,59 @@ export interface Endpoint<
   Output: O;
 }
 
+/**
+ * Data type representing an endpoint instance.
+ * @public getStaticPath accepts a formatting function (param: string) -> string and returns
+ **/
 export type EndpointInstance<E extends Endpoint<any, any, any, any, any, any>> = {
+  /**
+   * helper to get a path given a set of runtime params.
+   *
+   * @returns a string representation of a path instance
+   * @example
+   * ```
+   * import { Endpoint } from '..';
+   * import * as t from 'io-ts';
+   *
+   * const endpoint = Endpoint({
+   *  Input: {
+   *    Params: { id: t.number },
+   *  },
+   *  Method: 'GET',
+   *  getPath: ({ id }) => `users/${id}/crayons`,
+   *  Output: { crayons: t.array(t.string) },
+   *});
+   *
+   * endpoint.getPath({ id: 3 })
+   * // returns "users/3/crayons"
+   * ```
+   */
   getPath: E['getPath'];
+  /**
+   * helper to get a path version with static values in place of actual params.
+   *
+   * @returns a static representation of the path
+   * @example
+   * ```
+   * import { Endpoint } from '..';
+   * import * as t from 'io-ts';
+   *
+   * const endpoint = Endpoint({
+   *  Input: {
+   *    Params: { id: t.string },
+   *  },
+   *  Method: 'GET',
+   *  getPath: ({ id }) => `users/${id}/crayons`,
+   *  Output: { crayons: t.array(t.string) },
+   *});
+   *
+   * endpoint.getStaticPath(param => `:${param}`)
+   * // returns "users/:id/crayons"
+   * ```
+   */
+  getStaticPath: E['Input']['Params'] extends undefined
+    ? () => string
+    : (f: (paramName: string) => string) => string;
   Method: E['Method'];
   Output: t.ExactC<t.TypeC<E['Output']>>;
   Opts: EndpointOptions;
@@ -78,6 +130,7 @@ export type TypeOfEndpointInstance<E extends EndpointInstance<any>> = {
 
 /**
  * Constructor function for an endpoint
+ * @returns an EndpointInstance
  */
 export function Endpoint<
   M extends HTTPMethod,
@@ -90,6 +143,13 @@ export function Endpoint<
   return ({
     ...e,
     Output: t.strict(e.Output),
+    getStaticPath: (f: P extends undefined ? undefined : (paramName: string) => string) =>
+      e.getPath(
+        Object.keys(e.Input.Params ?? {}).reduce(
+          (acc, k) => ({ ...acc, [k]: (f ?? identity)(k) }),
+          {}
+        )
+      ),
     Input: {
       ...(e.Input.Body !== undefined ? { Body: t.strict(e.Input.Body as any) } : {}),
       ...(e.Input.Headers !== undefined ? { Headers: t.strict(e.Input.Headers as any) } : {}),
--- packages/ts-endpoint/src/Endpoint/__test__/endpoint.typetests.spec.ts
+++ packages/ts-endpoint/src/Endpoint/__test__/endpoint.typetests.spec.ts
@@ -99,6 +99,8 @@ describe('EndpointInstances typings are correct', () => {
 
   endpointInstance.Input.Params.type.props.id;
   endpointInstance.Input.Query.type.props.color;
+  endpointInstance.getStaticPath;
+  endpointInstance.getStaticPath;
   // @ts-expect-error
   endpointInstance.Input.Body?.prova;
   // @ts-expect-error
@@ -106,6 +108,94 @@ describe('EndpointInstances typings are correct', () => {
   endpointInstance.Output.type.props.crayons;
 
   it("wrong input won't compile", () => {
+    expect(true).toBeTruthy();
+  });
+});
+
+describe('getPath works as intended', () => {
+  const endpointWithParam = Endpoint({
+    Input: {
+      Query: { color: t.string },
+      Params: { id: t.number },
+    },
+    Method: 'GET',
+    getPath: ({ id }) => `users/${id.toString()}/crayons`,
+    Output: { crayons: t.array(t.string) },
+    Opts: { stringifyBody: true },
+  });
+
+  const endpointWithoutParam = Endpoint({
+    Input: {
+      Query: { color: t.string },
+    },
+    Method: 'GET',
+    getPath: () => `users/crayons`,
+    Output: { crayons: t.array(t.string) },
+    Opts: { stringifyBody: true },
+  });
+
+  it('adds the params correctly', () => {
+    expect(endpointWithParam.getPath({ id: 3 })).toEqual('users/3/crayons');
+    expect(endpointWithoutParam.getPath()).toEqual('users/crayons');
+  });
+  it('does not allow to pass incorrect params', () => {
+    // @ts-expect-error
+    () => endpointWithParam.getPath();
+    // @ts-expect-error
+    () => endpointWithParam.getPath({});
+    // @ts-expect-error
+    () => endpointWithParam.getPath({ foo: '' });
+    // @ts-expect-error
+    () => endpointWithParam.getPath({ id: '' });
+
+    expect(true).toBeTruthy();
+  });
+  it('does not allow to pass params if none is defined', () => {
+    // @ts-expect-error
+    () => endpointWithoutParam.getPath({});
+    // @ts-expect-error
+    () => endpointWithoutParam.getPath({ id: 2 });
+
+    expect(true).toBeTruthy();
+  });
+});
+
+describe('getStaticPath works as intended', () => {
+  const endpointWithParam = Endpoint({
+    Input: {
+      Query: { color: t.string },
+      Params: { id: t.number },
+    },
+    Method: 'GET',
+    getPath: ({ id }) => `users/${id.toString()}/crayons`,
+    Output: { crayons: t.array(t.string) },
+    Opts: { stringifyBody: true },
+  });
+
+  const endpointWithoutParam = Endpoint({
+    Input: {
+      Query: { color: t.string },
+    },
+    Method: 'GET',
+    getPath: () => `users/crayons`,
+    Output: { crayons: t.array(t.string) },
+    Opts: { stringifyBody: true },
+  });
+
+  it('adds the params correctly', () => {
+    expect(endpointWithParam.getStaticPath((param) => `:${param}`)).toEqual('users/:id/crayons');
+    expect(endpointWithoutParam.getStaticPath()).toEqual('users/crayons');
+  });
+  it('does not allow to pass incorrect params', () => {
+    // @ts-expect-error
+    () => endpointWithParam.getStaticPath();
+
+    expect(true).toBeTruthy();
+  });
+  it('does not allow to pass params if none is defined', () => {
+    // @ts-expect-error
+    () => endpointWithoutParam.getStaticPath((param) => `:${param}`);
+
     expect(true).toBeTruthy();
   });
 });
