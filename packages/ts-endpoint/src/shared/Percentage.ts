70046a3e4b814286258cd4aa54504350c1e22b82
fes300 <federicosordillo@gmail.com>
2020-07-22 14:47:34 +0200

    fix!: switch to simple percentage

---
+++ packages/ts-endpoint/src/shared/Percentage.ts
@@ -0,0 +1,13 @@
+import * as t from 'io-ts';
+
+interface PercentageBrand {
+  readonly Percentage: unique symbol;
+}
+
+export const Percentage = t.brand(
+  t.number,
+  (n): n is t.Branded<number, PercentageBrand> => n >= 0 && n <= 1,
+  'Percentage'
+);
+
+export type Percentage = t.TypeOf<typeof Percentage>;
