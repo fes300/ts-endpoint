186f9529ab0f30d54bf477b80944b8199815ece6
Andrea Ascari <dev.ascariandrea@gmail.com>
2021-01-01 23:32:08 +0100

    fix: endpoint body type inference

--- packages/ts-endpoint-express/src/__typetests__/addEndpoint.typespec.ts
+++ packages/ts-endpoint-express/src/__typetests__/addEndpoint.typespec.ts
@@ -4,7 +4,7 @@ import * as t from 'io-ts';
 import * as express from 'express';
 import { right } from 'fp-ts/lib/Either';
 
-const endpoint = Endpoint({
+const getEndpoint = Endpoint({
   Input: {
     Params: { id: t.string },
     Headers: { auth: t.string },
@@ -20,25 +20,41 @@ const router = express.Router();
 AddEndpoint();
 
 // @dts-jest:fail:snap won't compile if output oc controller is wrong
-AddEndpoint(router)(endpoint, ({ headers: { auth }, params: { id } }) => () => {
+AddEndpoint(router)(getEndpoint, ({ headers: { auth }, params: { id } }) => () => {
   console.log(auth, id);
   return Promise.resolve(right({ body: { crayons: [22] }, statusCode: 200 }));
 });
 
 // @dts-jest:fail:snap won't compile if trying to access non existent param
-AddEndpoint(router)(endpoint, ({ headers: { baz }, params: { id } }) => () => {
+AddEndpoint(router)(getEndpoint, ({ headers: { baz }, params: { id } }) => () => {
   console.log(baz, id);
   return Promise.resolve(right({ body: { crayons: ['brown'] }, statusCode: 200 }));
 });
 
 // @dts-jest:fail:snap won't compile if trying to access non defined body
-AddEndpoint(router)(endpoint, ({ headers: { auth }, params: { id }, body: { foo } }) => () => {
+AddEndpoint(router)(getEndpoint, ({ headers: { auth }, params: { id }, body: { foo } }) => () => {
   console.log(auth, id, foo);
   return Promise.resolve(right({ body: { crayons: ['brown'] }, statusCode: 200 }));
 });
 
 // @dts-jest:pass:snap correct constructions should work
-AddEndpoint(router)(endpoint, ({ headers: { auth }, params: { id } }) => () => {
+AddEndpoint(router)(getEndpoint, ({ headers: { auth }, params: { id } }) => () => {
   console.log(auth, id);
   return Promise.resolve(right({ body: { crayons: ['brown'] }, statusCode: 200 }));
+});
+
+
+const postEndpoint = Endpoint({
+  Input: {
+    Body: t.type({ content: t.string })
+  },
+  Method: 'POST',
+  getPath: ({ id }) => `users/${id}/crayons`,
+  Output: t.type({ crayons: t.array(t.string) }),
+});
+
+// @dts-jest:pass:snap correct constructions should work
+AddEndpoint(router)(postEndpoint, ({ body: { content } }) => () => {
+  console.log(content);
+  return Promise.resolve(right({ body: { crayons: ['brown'] }, statusCode: 201 }));
 });
